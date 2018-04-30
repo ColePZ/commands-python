04508d3a4b380072f378e1fc39d1b4af45483cfa
Cole Pazoid <34737903+ColePZ@users.noreply.github.com>
2018-04-30 13:13:30 +0100

    Public Main - Editable
    Editable by the community, some code will be loaded to Main.

---
+++ main_public.py
@@ -0,0 +1,17 @@
+command = ""
+print("PYCommands by TeamPZ")
+command = input("Command (Ensure That you have speech marks): ")
+
+if command == "Ping":
+ print("Pong")
+
+if command == "Help":
+ print("Commands: Help, Ping")
+
+if command == "Test":
+ print("PYCommands Ready for use")
+  
+
+
+
+ 
