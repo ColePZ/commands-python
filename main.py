ce337d8c39a237585b4bf02fd60ddae520e3d657
Cole Pazoid <34737903+ColePZ@users.noreply.github.com>
2018-04-30 13:11:54 +0100

    Main Data - Accessable by Cole Only
    This is a non-editable main data that will be loaded to YT and others, I will add to this.

---
+++ main.py
@@ -0,0 +1,18 @@
+command = ""
+print("PYCommands By TeamPZ")
+
+command = input("Command (Ensure That you have speech marks): ")
+
+if command == "Ping":
+ print("Pong")
+
+if command == "Help":
+ print("Commands: Help, Ping")
+
+if command == "Test":
+ print("PYCommands Ready for use")
+  
+
+
+
+ 
